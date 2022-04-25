49d6149163af885bb3f886af8bdd24c6360cea42
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2022-04-25 01:34:09 -0500

    create files

--- Backend/console.js
+++ Backend/console.js
@@ -1,12 +1,34 @@
-#!/user/bin/ node
+#!/usr/bin/node
 
 const cmd = require('node-cmd');
 const uiid = require('uuid/v4');
 const path = require('path');
 const shlex = require('shlex');
 const dataTime = require('node-datetime')
+const fs = require('fs');
+const { exec } = require('child_process');
+const storage = require('./models/__init__');
 
-// class HBNBCommand(cmd.Cmd)
 class HBNBCommand extends cmd.Cmd {
-    prompt = 'hbnb> ';
+    constructor() {
+        super();
+        this.prompt = 'hbnb> ';
+        this.commands = {
+            'help': this.help,
+            'quit': this.quit,
+        };
+    }
+
+    help() {
+        console.log('help');
+    }
+
+    quit() {
+        console.log('quit');
+        process.exit();
+    }
+
+    do_quit(line) {
+        this.quit();
+    }
 }
